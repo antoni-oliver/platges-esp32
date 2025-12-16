517d53c52795890daa6d9eda74539c08ac5b2d37
Antoni Oliver <antoni.oliver@uib.cat>
2025-12-16 13:51:36 +0100

    Credentials skeleton

---
+++ credentials.skeleton.h
@@ -0,0 +1,9 @@
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char *ssid = "__SSID__";
+const char *password = "__PASSWORD__";
+
+const char* serverName = "http://SERVER_NAME/PATH/";
+const char* formFieldName = "__FIELD_NAME__";
+const char* boundary = "__BOUNDARY__";
